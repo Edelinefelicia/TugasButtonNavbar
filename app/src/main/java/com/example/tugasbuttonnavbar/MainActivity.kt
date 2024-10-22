9a2b79b9b96f3192c729bde69810be5c87bc495e
Edelinefelicia <edelinedharmawan2906@gmail.com>
2024-10-23 03:03:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasbuttonnavbar/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.tugasbuttonnavbar
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.tugasbuttonnavbar.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding){
+            val navController = findNavController(R.id.nav_host_fragment)
+            bottomNavigationView.setupWithNavController(navController)
+
+        }
+    }
+
+
+}
